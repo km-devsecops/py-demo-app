8490b62653414a38f0dbf20a6b2ce1603d61b27a
Kiran Masani <kiran.masani@cybersecurityworks.com>
2022-09-11 22:12:28 +0530

    Updated Terraform scripts and Pipeline stages for infra-deploy

--- terraform-scripts/amplify.tf
+++ terraform-scripts/amplify.tf
@@ -1,3 +1,11 @@
 resource "aws_amplify_app" "demoenv" {
   name = "demoenv"
 }
+
+resource "aws_amplify_branch" "dev" {
+  app_id      = aws_amplify_app.demoenv.id
+  branch_name = "dev"
+
+  framework = "React"
+  stage     = "DEVELOPMENT"
+}
