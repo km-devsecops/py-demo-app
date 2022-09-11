e158b86577e35a101bf7c261a93fd057fa192d90
Kiran Masani <kiran.masani@cybersecurityworks.com>
2022-09-11 21:58:58 +0530

    Updated terraform scripts

--- terraform-scripts/vars.tf
+++ terraform-scripts/vars.tf
@@ -6,18 +6,5 @@ variable environment {
   default = "dev"
 }
 variable s3_kms_key {}
-variable ecr_kms_key {}
 
-variable vpc_id {
-  default = ""
-}
-
-variable subnet_ids {}
-variable tags {
-  type = map
-  default = {
-    "owner" = "devops@cybersecurityworks.com"
-    "terraform-spawned" = "true"
-  }
-}
 
